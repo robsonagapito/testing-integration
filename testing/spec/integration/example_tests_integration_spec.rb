b9c1e38d1bbc26168ad88f0bbc64c5a2b4a46b92
Robson Agapito <robson.agapito@locaweb.com.br>
2015-05-12 22:04:16 -0300

    ajustando Delete e Put

--- testing/spec/integration/example_tests_integration_spec.rb
+++ testing/spec/integration/example_tests_integration_spec.rb
@@ -6,16 +6,16 @@ require 'spec_helper'
 describe "Integration Tests" do
 
   before(:each) do
-
   end
   
   after(:each) do
   end
 
   it "Simple GET", :integ => false do
-    response = RestClient.get 'http://localhost:3000/users/1.json', {'Content-Type' => 'application/json'}
-    puts response.code
-    puts response.to_str
+    ###### EXECUTA O GET BUSCANDO O PRIMEIRO USUÁRIO ######
+    response = RestClient.get 'http://localhost:3000/users/1', :content_type => :json, :accept => :json
+    # puts response.code
+    # puts response.to_str
     resultado = JSON.parse(response)    
 
     expect(response.code).to eq(200)
@@ -23,32 +23,71 @@ describe "Integration Tests" do
   end
 
   it "Simple Post", :integ => false  do
-    param = {:user => {:login => "stellaagapito", :full_name => "Stella da Silva Agapito Correa", :email => "stellasilvaagapito@gmail.com", :age => "3"}}
-
+    param = {:user => 
+              {:login => "stellaagapito", 
+               :full_name => "Stella da Silva Agapito Correa", 
+               :email => "stellasilvaagapito@gmail.com", 
+               :age => "3"
+              }
+            }
     resp = RestClient.post 'http://localhost:3000/users', param, :content_type => :json, :accept => :json
     expect(resp.code).to eq(201)    
     result = JSON.parse(resp)
 
-    resp1 = RestClient.get "http://localhost:3000/users/#{result['id']}.json", {'Content-Type' => 'application/json'}
+    resp1 = RestClient.get "http://localhost:3000/users/#{result['id']}", :content_type => :json, :accept => :json
     result1 = JSON.parse(resp1) 
     expect(resp1.code).to eq(200)
     expect(result1["full_name"]).to eq("Stella da Silva Agapito Correa")
   end
 
-  it "Simple Delete", :integ => false  do
-    param = {:user => {:login => "stellaagapito", :full_name => "Stella da Silva Agapito Correa", :email => "stellasilvaagapito@gmail.com", :age => "3"}}
+  it "Simple Delete", :integ => true  do
+    param = {:user => 
+              {:login => "stellaagapito", 
+               :full_name => "Stella da Silva Agapito Correa", 
+               :email => "stellasilvaagapito@gmail.com", 
+               :age => "3"
+              }
+            }
 
     resp = RestClient.post 'http://localhost:3000/users', param, :content_type => :json, :accept => :json
     result = JSON.parse(resp)
 
-    resp1 = RestClient.get "http://localhost:3000/users/#{result['id']}.json", {'Content-Type' => 'application/json'}
+    resp1 = RestClient.get "http://localhost:3000/users/#{result['id']}", :content_type => :json, :accept => :json
     result1 = JSON.parse(resp1) 
     expect(result1["full_name"]).to eq("Stella da Silva Agapito Correa")
 
-    resp2 = RestClient.delete "http://localhost:3000/users/#{result1['id']}.json"
-
+    resp2 = RestClient.delete "http://localhost:3000/users/#{result1['id']}", :content_type => :json, :accept => :json
     expect(resp2.code).to eq(204)
 
+  end
+
+  it "Simple Put", :integ => false  do
+    param = {:user => 
+              {:login => "stellaagapito", 
+               :full_name => "Stella da Silva Agapito Correa", 
+               :email => "stellasilvaagapito@gmail.com", 
+               :age => "3"
+              }
+            }
+
+    resp = RestClient.post 'http://localhost:3000/users', param, :content_type => :json, :accept => :json
+    result = JSON.parse(resp)
+
+    resp1 = RestClient.get "http://localhost:3000/users/#{result['id']}", :content_type => :json, :accept => :json
+    result1 = JSON.parse(resp1) 
+    expect(result1["full_name"]).to eq("Stella da Silva Agapito Correa")
+
+    param = {:user => 
+              {:login => "stellaagapito", 
+               :full_name => "Stella Minha Filha Linda", 
+               :email => "stellasilvaagapito@gmail.com", 
+               :age => "3"
+              }
+            }    
+    resp2 = RestClient.put "http://localhost:3000/users/#{result1['id']}", param, :content_type => :json, :accept => :json
+    result2 = JSON.parse(resp2) 
+    expect(resp2.code).to eq(200)
+    expect(result2["full_name"]).to eq("Stella Minha Filha Linda")    
   end
 
 end
