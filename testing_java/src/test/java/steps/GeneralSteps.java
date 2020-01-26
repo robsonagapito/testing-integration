3c8491929f6e241d0d319b1ed16e422f7b0dfbac
Robson Agapito Correa <robsonagapito@gmail.com>
2020-01-26 15:47:43 -0300

    Update structure for Java Testing.

--- testing_java/src/test/java/steps/GeneralSteps.java
+++ testing_java/src/test/java/steps/GeneralSteps.java
@@ -5,6 +5,8 @@ import groovy.json.internal.LazyMap;
 import org.junit.Assert;
 import support.RESTSupport;
 
+import java.util.List;
+
 public class GeneralSteps {
 
     @Then("^user should see (.*) with value \"([^\"]*)\"$")
@@ -22,5 +24,19 @@ public class GeneralSteps {
         messageJson.put("unauthorized", 401);
 
         Assert.assertEquals(messageJson.get(type),RESTSupport.getResponseCode());
+    }
+
+    @Then("^user should see fields below:$")
+    public void userShouldSeeFieldsBelow(List<List<String>> table) {
+            System.out.println(table);
+            String field = "";
+            String value = "";
+            for (List<String> row: table){
+                field = row.get(0);
+                value = row.get(1);
+                Assert.assertEquals(value, RESTSupport.key(field));
+                System.out.println("Test case => " + row.get(0) + " >>> Field OK!");
+            }
+
     }
 }
--- testing_java/src/test/resources/features/starWars/star_wars_people.feature
+++ testing_java/src/test/resources/features/starWars/star_wars_people.feature
@@ -7,6 +7,14 @@ Feature: Example cucumber with Star Wars People
      And user should see name with value "Luke Skywalker"
      And user should see hair_color with value "blond"
 
+  Scenario: User would like to see information about Luke Skywalker with table
+    Given user would like to see people number 1
+    When user clicks on find people button into Star Wars series
+    Then user should see fields below:
+      |name       | Luke Skywalker |
+      |hair_color | blond          |
+     And user should see "success" message
+
   Scenario Outline: User would like to see status codes ok and not ok.
     Given user would like to see people number <codePeople>
     When user clicks on find people button into Star Wars series
